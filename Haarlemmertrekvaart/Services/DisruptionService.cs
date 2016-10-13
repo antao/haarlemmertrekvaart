8aac7d5a38d13fa0dbb9c36bb1fc885c0e91c9a9
Joao Antao <joaoantao@funda.nl>
2016-10-13 14:14:54 +0200

    disruptions - Add DisruptionService

---
+++ Haarlemmertrekvaart/Services/DisruptionService.cs
@@ -0,0 +1,14 @@
+﻿using Haarlemmertrekvaart.Clients;
+
+namespace Haarlemmertrekvaart.Services
+{
+    public class DisruptionService
+    {
+        private readonly NsClient _nsClient;
+
+        public DisruptionService(NsClient currentInstance)
+        {
+            _nsClient = currentInstance;
+        }
+    }
+}
