5bd7c86867c8abc1505ca1ce99773e0006cc3435
João Antão <joao.antao@protonmail.com>
2016-10-19 20:58:08 +0200

    http - Clean pack.ps1

--- NuGet/pack.ps1
+++ NuGet/pack.ps1
@@ -1,6 +1,6 @@
 $root = (split-path -parent $PSScriptRoot)
 
-Write-Host "ROOT - " + $root -ForegroundColor Green
+Write-Host "ROOT - " $root ForegroundColor Green
 
 $version = [System.Reflection.Assembly]::LoadFile("$root\Haarlemmertrekvaart\bin\Release\Haarlemmertrekvaart.dll").GetName().Version
 $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
@@ -9,7 +9,6 @@ Write-Host "Setting .nuspec version tag to $versionStr" -ForegroundColor Green
 
 $content = (Get-Content $root\NuGet\Haarlemmertrekvaart.nuspec) 
 $content = $content -replace '\$version\$',$versionStr
-
 $content | Out-File $root\nuget\Haarlemmertrekvaart.compiled.nuspec
 
 Invoke-Expression "$($root)\NuGet\NuGet.exe pack $root\nuget\Haarlemmertrekvaart.compiled.nuspec -Version $($version)"
