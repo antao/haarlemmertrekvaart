9d0aa4d38011f423106f0c368c069ba05be3b4dc
João Antão <joao.antao@protonmail.com>
2016-10-19 20:36:10 +0200

    http - NuGet path test (.gitignore needs update)

--- NuGet/pack.ps1
+++ NuGet/pack.ps1
@@ -1,18 +1,15 @@
 $root = (split-path -parent $PSScriptRoot)
 
-Write-Host "ROOT - " + $root -ForegroundColor Magenta
+Write-Host "ROOT - " + $root -ForegroundColor Green
 
 $version = [System.Reflection.Assembly]::LoadFile("$root\Haarlemmertrekvaart\bin\Release\Haarlemmertrekvaart.dll").GetName().Version
 $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 
-Write-Host "Setting .nuspec version tag to $versionStr" -ForegroundColor Magenta
+Write-Host "Setting .nuspec version tag to $versionStr" -ForegroundColor Green
 
 $content = (Get-Content $root\NuGet\Haarlemmertrekvaart.nuspec) 
 $content = $content -replace '\$version\$',$versionStr
 
 $content | Out-File $root\nuget\Haarlemmertrekvaart.compiled.nuspec
 
-Write-Host PS C:\> pathtest $($root)\NuGet" -ForegroundColor Magenta
-
-# $file= "Haarlemmertrekvaart.compiled.nuspec"
-# Invoke-Expression "$($root)\NuGet\NuGet.exe pack $($file) -Version $($version)"
+Invoke-Expression "$($root)\NuGet\NuGet.exe pack $root\nuget\Haarlemmertrekvaart.compiled.nuspec -Version $($version)"
